b3c153962be5b8441acf856f387d67d06fceec9c
Nic Vane <nicvane13@gmail.com>
2021-05-15 23:43:00 -0700

    updating code to be compatible with heroku hopefully

--- prove03.js
+++ prove03.js
@@ -21,6 +21,25 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+const cors = require("cors"); // Place this with other requires (like 'path' and 'express')
+const corsOptions = {
+  origin: "https://<your_app_name>.herokuapp.com/",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4,
+};
+
+const MONGODB_URL =
+  process.env.MONGODB_URL ||
+  "mongodb+srv://class-project:2011102778@cluster0.egvmm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 mongoConnect(() => {
   app.listen(PORT);
 });
